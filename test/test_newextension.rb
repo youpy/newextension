a388853e341092e52c644865dc02b1a2529e478c
youpy <youpy@ff3f829d-0f3b-0410-9483-87ee3ce16655>
2007-09-26 20:27:51 +0000

    newextension: initial import
    git-svn-id: http://svn.buycheapviagraonlinenow.com/repos/newextension@1 ff3f829d-0f3b-0410-9483-87ee3ce16655

---
+++ test/test_newextension.rb
@@ -0,0 +1,43 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+require 'fileutils'
+require 'pathname'
+
+include FileUtils
+
+class TestNewextension < Test::Unit::TestCase
+  def setup
+    @testdir = Pathname.new(File.dirname(__FILE__) + '/foo_extension')
+    system(Pathname.new(File.dirname(__FILE__)).realpath + '../bin/newextension',
+           '-a youpy',
+           '-V 0.11',
+           @testdir)
+  end
+
+  def teardown
+    rm_rf @testdir
+  end
+
+  def test_create
+    assert File.directory?(@testdir)
+    cd @testdir do
+      assert_equal([
+                    'chrome',
+                    'chrome/content',
+                    'chrome/content/FooExtension.js',
+                    'chrome/content/overlay.xul',
+                    'chrome/skin',
+                    'chrome.manifest',
+                    'install.rdf',
+                    'Rakefile',
+                   ], Dir['**/*'])
+
+      assert_file_contains(/FooExtension/, 'chrome/content/FooExtension.js')
+      assert_file_contains(/FooExtension/, 'chrome/content/FooExtension.js')
+      assert_file_contains(/chrome:\/\/foo_extension/, 'chrome/content/overlay.xul')
+      assert_file_contains(/chrome:\/\/foo_extension/, 'chrome.manifest')
+      assert_file_contains(/youpy/, 'install.rdf')
+      assert_file_contains(/0\.11/, 'install.rdf')
+      assert_file_contains(/EXTENSION_NAME="foo_extension"/, 'Rakefile')
+    end
+  end
+end
